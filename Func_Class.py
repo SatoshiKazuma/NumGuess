d9a3b11f2ff1bd5189db7201427320d8fbba87ab
SatoshiKazuma <sairamkumarm21@gmail.com>
2021-10-29 19:42:27 +0530

    rich

--- Func_Class.py
+++ Func_Class.py
@@ -1,4 +1,6 @@
 import random
+from rich.console import Console 
+console = Console()
 
 def Systempick(limit):
     return random.randint(0, limit)
@@ -8,15 +10,15 @@ def NumGuess(tries, guess_range, system_guess):
         user_guess = int(input('\nMake a guess: '))
         if user_guess in guess_range:
             if user_guess == system_guess:
-                print(f'\nYou won, with {i} try(s).')
+                console.print(f'\nYou won, with {i} try (s).',style='bold green')
                 return True
             elif user_guess > (system_guess + 10):
-                print(f'\nYour guess is too large, {(tries)-i} try(s) left.')
+                console.print(f'\nYour guess is too large, {(tries)-i} try (s) left.',style='red')
             elif user_guess < (system_guess - 10):
-                print(f'\nYour guess is too small, {(tries)-i} try(s) left.')
+                console.print(f'\nYour guess is too small, {(tries)-i} try (s) left.',style='red')
             elif user_guess in range(system_guess , (system_guess + 11)):
-                print(f'\nYour guess is larger, {(tries)-i} try(s) left.') 
+                console.print(f'\nYour guess is larger, {(tries)-i} try (s) left.',style='red') 
             elif user_guess in range((system_guess - 11) ,system_guess + 1):
-                print(f'\nYour guess is Smaller, {(tries)-i} try(s) left. ')
+                console.print(f'\nYour guess is Smaller, {(tries)-i} try (s) left. ',style='red')
         else:
-            print(f'Pick within the range, {(tries)-i} try(s) left.')
+            console.print(f'Pick within the range, {(tries)-i} try (s) left.',style='red')
--- app.py
+++ app.py
@@ -7,8 +7,8 @@ guess_range = range(0, guess_range_cap+1)
 system_guess = Systempick(guess_range_cap)
 tries = int(input('How many tries do you want: '))
 chances = (((guess_range_cap**(-1))*tries)*100)
-
+print(system_guess)
 if not NumGuess(tries, guess_range, system_guess):
-    print(f'\nYou could not guess the correct answer, the number was {system_guess} ')
+    console.print(f'\nYou could not guess the correct answer, the number was {system_guess}',style='red')
 
 print(f'Your chances of guessing right were at {round(chances, 4)}%')
